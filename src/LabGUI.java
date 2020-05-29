5e0ed2fe3ca6629d211f79820df6d2c105867220
Nevermind <1276411@bsu.edu.ru>
2020-05-30 02:29:16 +0300

    Изменено открытие файла

--- src/LabGUI.java
+++ src/LabGUI.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import javax.swing.*;
 import java.io.PrintWriter;
 import java.io.*;
-import java.util.Scanner;
 
 class JMenuTest extends JFrame{
     public static final long serialVersionUID = 1L;
     JTextField textField, textField2;
     private JLabel label;
+    private Desktop desktop = Desktop.getDesktop();
 
     public JMenuTest(){
         super("Лабораторная номер 9");
@@ -37,9 +37,10 @@ class JMenuTest extends JFrame{
         menuBar.add(createFileMenu());
         menuBar.add(createViewMenu());
         setJMenuBar(menuBar);
-        setSize(500, 500);
+        setSize(800, 500);
         setVisible(true);
     }
+
 
     private JMenu createFileMenu(){
         JMenu file = new JMenu("Файл");
@@ -57,11 +58,10 @@ class JMenuTest extends JFrame{
             public void actionPerformed(ActionEvent e){
                 System.out.println("ActionListener.actionPerformed: open");
 
-                JFileChooser fileChooser = new JFileChooser(".");
-                int userChoice = fileChooser.showOpenDialog(JMenuTest.this);
-                if(userChoice == JFileChooser.APPROVE_OPTION){
-                    String filename = fileChooser.getSelectedFile().getAbsolutePath();
-                    JOptionPane.showMessageDialog(JMenuTest.this, filename, "Файл, который вы выбрали", JOptionPane.PLAIN_MESSAGE);
+                JFileChooser fileChooser = new JFileChooser();
+                int returnVal = fileChooser.showOpenDialog(null);
+                if(returnVal == JFileChooser.APPROVE_OPTION){
+                    System.out.println("You chose to open this file: " + fileChooser.getSelectedFile().getName());
                 }
             }
         });
@@ -72,8 +72,8 @@ class JMenuTest extends JFrame{
                 try(FileWriter writer = new FileWriter("text.txt", false)) {
                     PrintWriter wr = new PrintWriter(writer);
 
-                    wr.println(textField.getText().getBytes());
-                    wr.println(textField2.getText().getBytes());
+                    wr.println(textField.getText());
+                    wr.println(textField2.getText());
 
                     wr.close();
                 } catch (IOException ex){
